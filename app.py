eb2738e1855142825e46ea08e474e3ac112d42f4
Cosmin Stoian <cosmin@bceng.tech>
2020-01-31 23:02:23 +0200

    Implemented module for determining profanity, lang at same time

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
-from textblob import TextBlob
-from profanity_check import predict, predict_prob
-from flask import Flask
-
-
-ro = TextBlob("Toate peste toate smartphone-ul are specificatii de tot, aproape toate componentele fiind high end, "
-              "mai putin display-ul")
-en = TextBlob("Waiting for this laptop and all the rumors were very exciting, but instead Apple has offered us this "
-              "piece of shit")
-print(ro.detect_language())
-print(en.detect_language())
-
-print('Profanity checking: ')
-print(predict(['fu ck it bro']))
-
+from flask import Flask, request, jsonify
+import profanity
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     if profanity.is_profanity('God damn gurl'):
+#         return 'E profanity'
+#     else:
+#         return 'Nu este profanity'
+
+
+@app.route('/decide', methods=['POST'])
+def is_profanity():
+    # if request.content_type != 'application/json':
+    #     raise BadRequest('Content-Type has to be application/json')
+    req = request.get_json()
+    txt = req.get('txt')
+    return jsonify(profanity.get_profanity(txt))
+
+
+@app.route('/prob', methods=['POST'])
+def prob():
+    # if request.content_type != 'application/json':
+    #     raise BadRequest('Content-Type has to be application/json')
+    req = request.get_json()
+    txt = req.get('txt')
+    return jsonify(profanity.get_profanity(txt, prob=True))
 
 
 if __name__ == '__main__':
